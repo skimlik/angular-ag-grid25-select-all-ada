003650fd92a57255c8297504d96ec43642458e6e
skimlik <kimlik.s@gmail.com>
2021-01-26 14:32:56 +0200

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { ColDef } from "ag-grid-community";
+import { IDataEntity } from "./data-entity";
 import { GridColumnFactory } from "./grid-columns.factory";
 
 @Component({
@@ -11,10 +12,28 @@ export class AppComponent {
   private gridApi;
   private gridColumnApi;
 
-  private columnDefs: ColDef[] = this.columnFactory.getColumns();
-  private defaultColDef;
-  private rowSelection;
-  private rowData: [];
+  columnDefs: ColDef[] = this.columnFactory.getColumns();
+  defaultColDef: ColDef = {};
+  rowSelection = true;
+  rowData: IDataEntity[] = [{
+    name: 'Alicia Coutts',
+    age: 24,
+    country: 'Australia',
+    year: 2012,
+    date: new Date(2012, 07, 12),
+    sport: 'Swimming',
+    gold: 1,
+    silver: 3,
+  }, {
+    name: 'Missy Franklin',
+    age: 17,
+    country: 'United States',
+    year: 2012,
+    date: new Date(2012, 07, 12),
+    sport: 'Swimming',
+    gold: 4,
+    silver: 0,
+  }];
 
   constructor(private columnFactory: GridColumnFactory) {
     this.defaultColDef = {
---
+++ src/app/data-entity.ts
@@ -0,0 +1,10 @@
+export interface IDataEntity {
+  name: string;
+  age: number;
+  country: string;
+  year: number;
+  date: Date;
+  sport: string;
+  gold: number;
+  silver: number;
+}
